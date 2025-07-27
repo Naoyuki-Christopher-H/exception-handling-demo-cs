efc6b45e54b137ee7099b0ed1cc159f0961d5fac
YukiChriss <naoyuki.christopher.higaki@outlook.com>
2025-07-27 14:22:57 +0200

    Add project files.

---
+++ exception-handling-demo-cs/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception_handling_demo_cs
+{
+    // Exception Handling: Errors that occur during execution.
+    // 1. Try: Try to execute code that may throw an exception.
+    // 2. Catch: Handle the exception if it occurs.
+    // 3. Finally: Code that runs regardless of whether an exception occurred or not.
+    // Note: The finally block is optional and can
+    // be used to clean up resources or perform actions that should always run.
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution Soultion = new Solution();
+        }
+    }
+}
---
+++ exception-handling-demo-cs/Solution.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exception_handling_demo_cs
+{
+    class Solution
+    {
+        public Solution()
+        {
+            try
+            {
+                Double Value_1;
+                Double Value_2;
+                Double Value_3;
+
+                Console.WriteLine("Enter the first number:");
+                Value_1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Enter the second number:");
+                Value_2 = Convert.ToDouble(Console.ReadLine());
+
+                Value_3 = Value_1 / Value_2;
+
+                Console.WriteLine($"RESULT: {Value_3}");
+            }
+            catch (FormatException Issue)
+            {
+                Console.WriteLine($"ERROR: {Issue.Message}");
+                Console.WriteLine($"NOTE: Number's only.");
+                Console.Write($"VALUE: ");
+            }
+            catch (DivideByZeroException Issue)
+            {
+                Console.WriteLine($"ERROR: {Issue.Message}");
+                Console.WriteLine($"NOTE: You can not divide by zero.");
+                Console.Write($"VALUE: ");
+            }
+            catch (Exception Issue)
+            {
+                Console.WriteLine($"ERROR: {Issue.Message}");
+                Console.Write($"VALUE: ");
+            }
+        }
+    }
+}
